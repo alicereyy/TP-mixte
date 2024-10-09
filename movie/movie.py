d15c5e2c4dc8b168e2b0e607c7f904b802e99b0e
alicereyy <128583531+alicereyy@users.noreply.github.com>
2024-10-09 10:07:12 +0200

    add methods to movie
    now same methods as in movie in TP REST

--- movie/movie.py
+++ movie/movie.py
@@ -12,12 +12,16 @@ type_defs = load_schema_from_path('movie.graphql')
 
 query = QueryType()
 query.set_field('movie_with_id', r.movie_with_id)
+query.set_field('movie_with_title', r.movie_with_title)
+query.set_field('movie_with_director', r.movie_with_director)
 
 movie = ObjectType('Movie')
 movie.set_field('actors', r.resolve_actors_in_movie)
 
 mutation = MutationType()
 mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('add_movie', r.add_movie)
+mutation.set_field('delete_movie', r.delete_movie)
 
 actor = ObjectType('Actor')
 
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -6,6 +6,20 @@ def movie_with_id(_,info,_id):
         for movie in movies['movies']:
             if movie['id'] == _id:
                 return movie
+
+def movie_with_title(_,info,_title):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['title'] == _title:
+                return movie
+
+def movie_with_director(_,info,_director):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['director'] == _director:
+                return movie
             
 def update_movie_rate(_,info,_id,_rate):
     newmovies = {}
@@ -21,6 +35,33 @@ def update_movie_rate(_,info,_id,_rate):
         json.dump(newmovies, wfile)
     return newmovie
 
+def add_movie(_,info,_id,_title,_director,_rating):
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                return "error: movie ID already exists"
+    newmovie = {
+        "title": _title,
+        "rating": _rating,
+        "director": _director,
+        "id": _id
+    }
+    movies['movies'].append(newmovie)
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(movies, wfile)
+    return "movie added"
+
+def delete_movie(_,info,_id):
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                movies['movies'].remove(movie)
+                with open('{}/data/movies.json'.format("."), "w") as wfile:
+                    json.dump(movies, wfile)
+                return "movie deleted"
+    return "error: movie ID not found"
 
 def resolve_actors_in_movie(movie, info):
     with open('{}/data/actors.json'.format("."), "r") as file:
