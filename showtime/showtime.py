deefff35e4d9e52ef432cd6275872a7348809694
alicereyy <128583531+alicereyy@users.noreply.github.com>
2024-10-09 11:34:06 +0200

    add gRPC showtime service

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -9,6 +9,19 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
     def __init__(self):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
+    
+    def GetSchedule(self, request, context):
+        for schedule in self.db:
+            yield showtime_pb2.Schedule(date=schedule['date'], movies=schedule['movies'])
+    
+    def GetMoviesOnDate(self, request, context):
+        for schedule in self.db:
+            if schedule['date'] == request.date:
+                print("Movies found on this date!")
+                return showtime_pb2.Schedule(date=schedule['date'], movies=schedule['movies'])
+        print("No movies on this date")
+        return showtime_pb2.Schedule(date=request.date, movies=[])
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
