85a3b3259ee3ce65b35acb7c8f09069e5505f63b
alicereyy <128583531+alicereyy@users.noreply.github.com>
2024-10-19 12:37:28 +0200

    Revert "Merge branch 'VF1' into VF2_alice"
    This reverts commit e4aca781a8a29348dfa365f3696c80b89faa35d7, reversing
    changes made to aeb213273422bc11c91d6fc1e59915a624ace7bb.

--- showtime/showtime_pb2_grpc.py
+++ showtime/showtime_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import showtime_pb2 as showtime__pb2
 
-GRPC_GENERATED_VERSION = '1.66.1'
+GRPC_GENERATED_VERSION = '1.66.2'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
