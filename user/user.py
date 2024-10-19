3e6e39c1f338dcadd559e7730e3472f0eeb7ccd8
alicereyy <128583531+alicereyy@users.noreply.github.com>
2024-10-19 11:59:19 +0200

    fix/update user
    method using a test client

--- user/user.py
+++ user/user.py
@@ -49,9 +49,12 @@ def get_booking_for_userid(userid):
                            'date': date_movie.date,
                            'movies': list(date_movie.movies)
                      })
-               return make_response(jsonify(user_bookings), 200)
+               if user_bookings==[]:
+                  return make_response(jsonify({"error": "This user does not have any bookings"}), 404)
+               else:
+                  return make_response(jsonify(user_bookings), 200)
             except grpc.RpcError as e:
-               return make_response(jsonify({"error": "This user does not have any bookings"}), 404)
+               return make_response(jsonify({"error": "Call to the booking service failed"}), 503)
    return(make_response(jsonify({"error": "This user does not exist in the users database"}), 400))
 
 # Returns the booked movies of a user on the chosen date 
@@ -73,15 +76,30 @@ def get_booking_by_date_user(userid, date):
                         }), 200)
                   return make_response(jsonify({"error": "There are no bookings on this date for this user"}), 409)
             except grpc.RpcError as e:
-                  return make_response(jsonify({"error": "This user does not have any bookings"}), 404)
+                  return make_response(jsonify({"error": "Call to the booking service failed"}), 503)
    return make_response(jsonify({"error": "This user does not exist in the users database"}), 400)
 
-def getMovieInfo(movieid) :
-   variables = {"id": movieid}
-    
+def getMovieInfo(movieid) :   
+   query = '''
+   {
+      movie_with_id(_id: "{movieid}") {
+         id
+         title
+         director
+         rating
+         actors {
+            id
+            firstname
+            lastname
+            birthyear
+         }
+      }
+   }
+   '''.replace("{movieid}", movieid)
+
    response = requests.post(
-      'http://127.0.0.1:3200/graphql',
-      json={'query': '{ movie_with_id(_id: "_id") { id title director rating actors { id firstname lastname birthyear } } }'.replace("_id", movieid)}
+      'http://172.16.131.192:3001/graphql',
+      json={'query': query}
    )
     
    if response.status_code == 200:
@@ -93,31 +111,39 @@ def getMovieInfo(movieid) :
 @app.route("/users/movie_info/<userid>", methods=['GET'])
 def get_movies_info_for_user_bookings(userid):
     
-   user_bookings = get_booking_for_userid(userid)
-    
-   if not user_bookings:
-      return make_response(jsonify({"error": "This user does not have any bookings"}), 404)
+   #user_bookings, status = get_booking_for_userid(userid)
+   
+   # Create a test client to simulate an internal request to get_booking_for_userid
+    with app.test_client() as client:
+      # Simulate a GET request to the /users/<userid> route
+      response = client.get(f"/users/{userid}")
+      
+      # Get the JSON data and status code from the response
+      user_bookings = response.get_json()
+      status = response.status_code
+        
+      if status != 200:
+         return response
 
-   movies_infos = []
-    
-   for user_booking in user_bookings:
-      for date_movie in user_booking.dates:
+      movies_infos = []
+      
+      for user_booking in user_bookings:
          booking_info = {
-               'date': date_movie.date,  
+               'date': user_booking['date'],  
                'movies': []
          }
-         
-         for movieid in user_booking.movies:
+         for movieid in user_booking['movies']:
+            print("movie id:" + movieid)
             movie_info = getMovieInfo(movieid)
             if movie_info:
                booking_info['movies'].append(movie_info)
-
             else:
                return make_response(jsonify({"error": f"Movie with ID {movieid} does not exist in the movies database"}), 404)
-
+         
          movies_infos.append(booking_info)
 
-   return make_response(jsonify(list(movies_infos)), 200)
+      return make_response(jsonify(list(movies_infos)), 200)
+
 
 
 if __name__ == "__main__":
